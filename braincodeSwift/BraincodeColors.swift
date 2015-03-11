3165719543d2502e2a087559474b50025d1a349f
ewa <ewa.bielska2@gmail.com>
2015-03-11 20:48:25 +0100

    Registration layout

--- braincodeSwift/BraincodeColors.swift
+++ braincodeSwift/BraincodeColors.swift
@@ -12,15 +12,15 @@ import UIKit
 extension UIColor {
     
     class func lightBlue() -> UIColor {
-        return UIColor(red: 143/255.0, green: 70/255.0, blue: 121/255.0, alpha: 0.5);
+        return UIColor(red: 143/255.0, green: 70/255.0, blue: 121/255.0, alpha: 0.4);
     }
     
     class func blue() -> UIColor {
-        return UIColor(red: 95/255.0, green: 12/255.0, blue: 127/255.0, alpha: 0.5);
+        return UIColor(red: 95/255.0, green: 12/255.0, blue: 90/255.0, alpha: 0.4);
     }
     
     class func darkBlue() -> UIColor {
-        return UIColor(red: 6/255.0, green: 7/255.0, blue: 172/255.0, alpha: 0.5);
+        return UIColor(red: 6/255.0, green: 7/255.0, blue: 110/255.0, alpha: 0.4);
     }
     
 }
